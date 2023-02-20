3dda057080b323d1c8c6298bb1eba35571a755a2
dtonlai <dtonlai@ualberta.ca>
2023-02-20 13:47:35 -0700

    Added rating, menu item, and absolute positioning to card.

--- client/screens/MainView.js
+++ client/screens/MainView.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { StatusBar, StyleSheet, View, Dimensions, Image } from "react-native";
-import { Card, FAB, Text } from "react-native-paper";
+import { Card, Chip, FAB, Text, Banner } from "react-native-paper";
 import MapView from "react-native-maps";
 import { Marker } from "react-native-maps";
 
 import ramen from "../assets/ramen.png";
 
 function MainView() {
+  const [bannerVisible, setBannerVisible] = useState(true);
+
   return (
     <View style={styles.container}>
       {/* Start Map Component */}
@@ -34,12 +36,41 @@ function MainView() {
       {/* End Map Component */}
 
       {/*  Start Card Component */}
+
       <View style={styles.card}>
-        <Image
-          source={require("../assets/ramen.png")}
-          style={{ height: 150, width: 150 }}
-        />
-        <Text variant="titleLarge">Shoyu Ramen</Text>
+        <View style={styles.cardImage}>
+          <Image
+            source={require("../assets/ramen.png")}
+            style={{ height: 125, width: 125 }}
+          />
+        </View>
+
+        <View style={styles.cardContent}>
+          <Text variant="titleLarge">Menya Musashi</Text>
+          <Text variant="bodyMedium">Shoyu Ramen</Text>
+          <View style={styles.cardRating}>
+            <Image
+              source={require("../assets/naruto-black.png")}
+              style={{ height: 35, width: 35 }}
+            />
+            <Image
+              source={require("../assets/naruto-black.png")}
+              style={{ height: 35, width: 35 }}
+            />
+            <Image
+              source={require("../assets/naruto-black.png")}
+              style={{ height: 35, width: 35 }}
+            />
+            <Image
+              source={require("../assets/naruto-black.png")}
+              style={{ height: 35, width: 35 }}
+            />
+            <Image
+              source={require("../assets/naruto-black.png")}
+              style={{ height: 35, width: 35 }}
+            />
+          </View>
+        </View>
       </View>
 
       {/* End Card Component */}
@@ -64,18 +95,38 @@ const styles = StyleSheet.create({
   },
   map: {
     width: Dimensions.get("window").width,
-    height: "50%",
+    height: "100%",
   },
   card: {
+    padding: 10,
     flex: 1,
+    flexDirection: "row",
     backgroundColor: "#FFFFFF",
     width: Dimensions.get("window").width,
-    height: "50%",
+    height: "25%",
+    position: "absolute",
+    bottom: 0,
+    margin: 75,
+    borderRadius: 5,
+  },
+  cardImage: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  cardContent: {
+    display: "flex",
+    justifyContent: "space-around",
+    height: "100%",
+  },
+  cardRating: {
+    flex: 1,
+    flexDirection: "row",
   },
   fab: {
     position: "absolute",
     margin: 16,
-    right: 0,
+    // right: 0,
     bottom: 0,
   },
 });
